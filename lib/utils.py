e7aefe944e8ca8a97200d2c597f16cfb609db72d
Haokun Chen <chenhaokun24549@gmail.com>
2023-02-20 15:33:28 +0100

    a

--- lib/utils.py
+++ lib/utils.py
@@ -40,7 +40,6 @@ def _random_seeder(seed):
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    torch.cuda.set_device('cuda')
     torch.cuda.manual_seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
