100f10fc64d17ce9a015ebd5ff9787ab3a338751
Haokun Chen <chenhaokun24549@gmail.com>
2023-03-02 15:31:24 +0100

    ad

--- train.sh
+++ train.sh
@@ -12,3 +12,11 @@ CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.001 --alg fedavg --dataset d
 CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.001 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 1 --feature_iid 1 --label_iid 0 --alpha 1 >digit_fedavg_filn_1bb_5u_a1.log
 CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.001 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 1 --feature_iid 0 --label_iid 1 --alpha 1 >digit_fedavg_fnli_1bb_5u_a1.log
 
+CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.01 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 3 --feature_iid 0 --label_iid 0 --alpha 1 >digit_fedavg_fnln_3bb_5u_a1_001.log
+CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.01 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 3 --feature_iid 1 --label_iid 0 --alpha 1 >digit_fedavg_filn_3bb_5u_a1_001.log
+CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.01 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 3 --feature_iid 0 --label_iid 1 --alpha 1 >digit_fedavg_fnli_3bb_5u_a1_001.log
+
+CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.01 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 1 --feature_iid 0 --label_iid 0 --alpha 1 >digit_fedavg_fnln_1bb_5u_a1_001.log
+CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.01 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 1 --feature_iid 1 --label_iid 0 --alpha 1 >digit_fedavg_filn_1bb_5u_a1_001.log
+CUDA_VISIBLE_DEVICES=2 python haokun/main.py --lr 0.01 --alg fedavg --dataset digit --num_users 5 --rounds 200 --num_bb 1 --feature_iid 0 --label_iid 1 --alpha 1 >digit_fedavg_fnli_1bb_5u_a1_001.log
+
