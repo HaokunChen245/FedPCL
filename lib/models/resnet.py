1d6757e17796e97d875dae39a56c86102f76fbf3
Haokun Chen <chenhaokun24549@gmail.com>
2023-03-02 14:52:33 +0100

    s

--- lib/models/resnet.py
+++ lib/models/resnet.py
@@ -115,8 +115,8 @@ class ResNetWrapper(nn.Module):
             net_curr.to('cuda')            
         # self.fc = nn.Linear(512 * len(self.nets), num_classes).to('cuda')
         # self.fc = nn.Linear(self.nets[0].num_classes * len(self.nets), num_classes).to('cuda')
-        for p in self.fc.parameters():                
-            p.requires_grad = True
+        # for p in self.fc.parameters():                
+        #     p.requires_grad = True
                    
     def forward(self, x, get_features=False):
         # fs = []
